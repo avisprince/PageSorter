4027e4e89040ae7def1812c09a0dd75ee73005f3
Avi Prince <Avi Prince>
2017-01-26 02:01:04 -0500

    delete pdf files

--- PageSorterService/PageSorterService/Controllers/TestController.cs
+++ PageSorterService/PageSorterService/Controllers/TestController.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using ImageMagick;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+using System.IO;
 using System.Web.Http;
 
 namespace PageSorterService.Controllers
@@ -14,6 +13,44 @@ namespace PageSorterService.Controllers
         public string Name()
         {
             return "Angular";
+        }
+
+        [HttpGet]
+        [Route("api/pdf")]
+        public List<string> Pdf()
+        {
+            var path = @"C:\Users\avisp\Documents\Visual Studio 2015\Projects\TestPDFSplit\TestPDFSplit\PDF Files\";
+
+            var settings = new MagickReadSettings()
+            {
+                Density = new Density(300, 300),
+            };
+
+            var pdfImageList = new List<string>();
+
+            using (var images = new MagickImageCollection())
+            {
+                // Add all the pages of the pdf file to the collection
+                images.Read(path + "testPDF2.pdf", settings);
+
+                int page = 1;
+                foreach (var image in images)
+                {
+                    // Write page to file that contains the page number
+                    //image.Write(path + "DummyPdf_page" + page + ".png");
+
+                    image.Format = MagickFormat.Png;
+
+                    using (var ms = new MemoryStream())
+                    {
+                        image.Write(ms);
+
+                        pdfImageList.Add(Convert.ToBase64String(ms.ToArray()));
+                    }
+                }
+            }
+
+            return pdfImageList;
         }
     }
 }
